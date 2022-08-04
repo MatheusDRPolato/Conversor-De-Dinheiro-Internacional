ef1624f619e9d2760279bac4d68f93ebbeea6aec
Matheus Dos Reis Polato <99278233+MatheusDRPolato@users.noreply.github.com>
2022-08-04 17:28:05 -0300

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,7 @@
+reais = float(input("Quantidade de dinheiro em REAIS para a viagem:"))
+d = reais/5.80
+print(f"Ele tera US$ {d:.2f}")
+ma = reais/3.00
+print(f"Ele tera DEM {ma:.2f} de marcoalemao")
+le = reais/6.57
+print(f"Ele tera £ {le:.2f} de libra esterlina")
